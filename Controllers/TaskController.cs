779522ead36690c4257b27d5674ecc7744a72014
Hayato Yokoyama <hayatoyokoyama@gmail.com>
2021-03-19 10:33:05 -0700

    Added assets

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TicketTracker.Controllers
+{
+    public class TaskController : Controller
+    {
+        // GET: Task
+        public ActionResult Task()
+        {
+
+            return PartialView();
+        }
+    }
+}
